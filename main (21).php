9fc8695768cebf25c552d3ef4f9ff7387b3324ea
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:41:08 +0300

    Add files via upload

---
+++ main (21).php
@@ -0,0 +1,21 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// PHP FONKSIYONLARA GIRIS
+// ENDIREK OZYINELEMELI FONKSIYONLAR
+
+function islem($sayi) {
+    foreach ($sayi as $rakam) {
+        echo($rakam);
+    }
+}
+islem(array("1","2","3","4"));
+
+
+
+
+
+?>
